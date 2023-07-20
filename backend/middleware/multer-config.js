f0be684ea71feb66221cd3f07910fa3132f8a02a
Mathieu Dourlens <matdourlens95@gmail.com>
2023-07-21 00:21:51 +0200

    Supression de commentaire

--- backend/middleware/multer-config.js
+++ backend/middleware/multer-config.js
@@ -1,7 +1,5 @@
-
 const multer = require("multer");
 
-//multer facilite la gestion des fichiers envoyés avec des requêtes http vers l'API
 const fileFilter = function (req, file, callback) {
   const fileSupported = ["image/jpeg", "image/jpg", "image/png"];
   if (!fileSupported.includes(file.mimetype)) {
@@ -16,28 +14,5 @@ const storage = multer.memoryStorage();
 
 const upload = multer({ storage: storage, fileFilter: fileFilter });
 
-//un fichier unique qui est une image
 module.exports = upload.single("image");
 
-
-/*const multer = require('multer');
-
-const MIME_TYPES = {
-  'image/jpg': 'jpg',
-  'image/jpeg': 'jpg',
-  'image/png': 'png'
-};
-
-const storage = multer.diskStorage({
-  destination: (req, file, callback) => {
-    callback(null, 'images');
-  },
-  filename: (req, file, callback) => {
-    const name = file.originalname.split(' ').join('_');
-    const extension = MIME_TYPES[file.mimetype];
-    callback(null, name + Date.now() + '.' + extension);
-  }
-});
-
-module.exports = multer({storage: storage}).single('image');
-*/
